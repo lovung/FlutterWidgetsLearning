3c4928dc118bc9d255bbc8afce653415b02cbdee
Vũ Long <vulong1602@gmail.com>
2020-11-16 21:14:11 +0700

    Safe Area + Expanded + Notification

--- lib/expanded/expanded.dart
+++ lib/expanded/expanded.dart
@@ -13,11 +13,14 @@ class ExpandedExample extends StatelessWidget {
           child: Row(
             children: [
               MyWidget(),
-              Expanded(
-                flex: 2,
-                child: MyWidget(),
+              Column(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: MyWidget(),
+                  ),
+                ],
               ),
-              MyWidget(),
               Expanded(
                 flex: 1,
                 child: MyWidget(),
@@ -34,7 +37,6 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(10),
       width: 50,
       height: 50,
       color: Colors.yellow.shade700,
--- lib/safe_area/safe_area.dart
+++ lib/safe_area/safe_area.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:overlay_support/overlay_support.dart';
 
 /// Providers are declared globally and specifies how to create a state
-final counterProvider = StateProvider((ref) => 0);
+var counterProvider = StateProvider((ref) => 0);
 
 // A widget that insets its child by sufficient padding to avoid intrusions by the operating system.
 // For example, this will indent the child by enough to avoid the status bar at the top of the screen.
@@ -14,33 +13,9 @@ class SafeAreaExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Safe Area example')),
-        body: Center(
-          // Consumer is a widget that allows you reading providers.
-          // You could also use the hook "useProvider" if you uses flutter_hooks
-          child: Consumer(builder: (context, watch, _) {
-            final count = watch(counterProvider).state;
-            return Text('$count', style: TextStyle(fontSize: 24));
-          }),
-        ),
-        floatingActionButton: FloatingActionButton(
-          // The read method is an utility to read a provider without listening to it
-          onPressed: () => showSimpleNotification(
-            Text("Subscribe to FilledStacks"),
-            background: Colors.purple,
-            autoDismiss: false,
-            trailing: Builder(builder: (context) {
-              return FlatButton(
-                  textColor: Colors.yellow,
-                  onPressed: () {
-                    OverlaySupportEntry.of(context).dismiss();
-                  },
-                  child: Text('Dismiss'));
-            }),
-          ),
-          child: const Icon(Icons.add),
-        ),
+      child: Material(
+        child: Text(
+            "A widget that insets its child by sufficient padding to avoid intrusions by the operating system."),
       ),
     );
   }
